b2394283925d99bc4f2f02325f131c0adc1cf6fe
Tyler Tierney <tytierney@yahoo.com>
2021-08-24 14:58:45 -0400

    Fixing build-time errors (crossOrigin props)

--- components/Logo/Logo.js
+++ components/Logo/Logo.js
@@ -7,7 +7,7 @@ const Logo = () => {
     <>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap"
           rel="stylesheet"
--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
-class MyDocument extends Document {
+class Document extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
     return { ...initialProps };
@@ -11,7 +11,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
             href="https://fonts.googleapis.com/css2?family=Fredoka+One&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600&display=swap"
             rel="stylesheet"
@@ -26,4 +26,4 @@ class MyDocument extends Document {
   }
 }
 
-export default MyDocument;
+export default Document;
