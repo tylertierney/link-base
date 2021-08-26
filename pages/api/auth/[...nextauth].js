f94ac5e4ab6b147666e31a5610065f2ed2967b67
Tyler Tierney <tytierney@yahoo.com>
2021-08-26 16:10:29 -0400

    Trying out other authentication stuff

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,13 +5,37 @@ export default NextAuth({
   // Configure one or more authentication providers
 
   providers: [
+    Providers.Credentials({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "Username" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const res = await fetch("http://localhost:3000", {
+          method: "POST",
+          body: JSON.stringify(credentials),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        const user = await res.json();
+
+        if (res.ok && user) {
+          return user;
+        }
+
+        return null;
+      },
+    }),
     Providers.GitHub({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
     // ...add more providers here
   ],
-
-  // A database is optional, but required to persist accounts in a database
   database: process.env.MONGODB_URI,
+  session: {
+    jwt: true,
+  },
 });
